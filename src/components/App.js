ac3c4831e4ce79abdd031b8c21e9d8a4ae173a88
Kyle Miller <millerkyle85@gmail.com>
2022-09-12 16:55:55 -0700

    import weatherData

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WeatherData from './WeatherData';
+
+function App() {
+  return (
+    <React.Fragment>
+      < WeatherData />
+    </React.Fragment>
+  );
+}
+
+export default App;
