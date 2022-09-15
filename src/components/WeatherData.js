7f412af168b3a65938600abd8d96236307e165b6
Kyle Miller <millerkyle85@gmail.com>
2022-09-15 01:54:59 -0700

    made btns to quick search

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,79 +1,95 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function WeatherData() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [data, setData] = useState([]);
-  const [location, setLocation] = useState("Los angeles");
+  const [location, setLocation] = useState('');
+
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+
+  const londonURL = `https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+
+  const parisURL = `https://api.openweathermap.org/data/2.5/weather?q=Paris&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+      })
+      setLocation('')
+    }
+  }
+  const londonClick = () => {
+    axios.get(londonURL).then((response) => {
+      setData(response.data)
+    })
+      setLocation('London')
+  }
+  const parisClick = () => {
+    axios.get(parisURL).then((response) => {
+      setData(response.data)
+    })
+      setLocation('Paris')
+  }
   
-  useEffect(() => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`)
-      .then(response => {
-          if (!response.ok) {
-            throw new Error(`${response.status}: ${response.statusText}`);
-          } else {
-            return response.json()
-          }
-        })
-      .then((jsonifiedResponse) => {
-          setData(jsonifiedResponse)
-          setIsLoaded(true)
-        })
-      .catch((error) => {
-        setError(error.message)
-        setIsLoaded(true)
-      });
-    }, [])
-    if (error) {
-      return <h1>Error: {error}</h1>;
-    } else if (!isLoaded) {
-      return <h1>...Loading...</h1>;
-    } else {
-      return (
-        <React.Fragment>
-        <div className="app">
-          <div className="search">
-            <input
-              value={location}
-              onChange={event => setLocation(event.target.value)}
-              placeholder='Enter Location'
-              type="text"
-              />
-          </div>
-            <div className="container">
-              <div className="top">
-                <div className="location">
-                  <p>{data.name}</p>
-                </div>
-                <div className="temp">
-                  <h1>{data.main.temp.toFixed()}°F</h1>
-                </div>
-                
-                {data["weather"].map((weatherData, index) =>
-                  <div key={index}>
-                    <p>{weatherData["description"]}</p>
-                  </div>
-                )}
+
+    return (
+      <React.Fragment>
+      <div className="app">
+        <div className="searchBTN">
+          <button type="button"
+            onClick={() => londonClick()}
+            >London</button>
+            <button type="button"
+            onClick={() => parisClick()}
+            >Paris</button>
+            <button type="button"
+            onClick={() => setLocation('Los Angeles')}
+            >Los Angeles</button>
+            <button type="button"
+            onClick={() => setLocation('New York')}
+            >New York</button>
+        </div>
+        <div className="search">
+          <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="text"
+            />
+        </div>
+          <div className="container">
+            <div className="top">
+              <div className="location">
+                <p>{data.name}</p>
               </div>
-              <div className="bottom">
-                <div className="humidity">
-                  <p>{data.main.humidity}%</p>
-                  <p>Humidity</p>
-                </div>
-                <div className="feels">
-                  <p>{data.main.feels_like.toFixed()}°F</p>
-                  <p>Feels Like</p>
-                </div>
-                <div className="speed">
-                  <p>{data.wind.speed.toFixed()} MPH</p>
-                  <p>Wind Speed</p>
-                </div>
+              <div className="temp">
+                {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+              </div>
+              <div className="description">
+                {data.weather ? <p>{data.weather[0].main}</p> : null}
               </div>
             </div>
-        </div>
-        </React.Fragment>
-      );
-    }
+            <div className="bottom">
+              <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+                <p>Humidity</p>
+              </div>
+              <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="speed">
+                {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          </div>
+      </div>
+      </React.Fragment>
+    );
 }
 
 export default WeatherData;
