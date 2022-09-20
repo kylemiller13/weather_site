2c383ea6b67a2572d20fed0fc925553eec6a4bc6
Kyle Miller <millerkyle85@gmail.com>
2022-09-20 01:49:48 -0700

    display second api date, degree, icon

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,22 +1,52 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { UilTemperatureThreeQuarter, 
+  UilWind, 
+  UilTear 
+} from '@iconscout/react-unicons';
+
+
+
 
 function WeatherData() {
   const [data, setData] = useState([]);
   const [location, setLocation] = useState('');
+  const [futureData, setFutureData] = useState([]);
+  
 
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
 
   const londonURL = `https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
 
-  const parisURL = `https://api.openweathermap.org/data/2.5/weather?q=Paris&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+  const tokyoURL = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+
+  const losAngelesURL = `https://api.openweathermap.org/data/2.5/weather?q=Los Angeles&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+
+  const newYorkURL = `https://api.openweathermap.org/data/2.5/weather?q=New York&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+
+  const fiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&exclude={minutely}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+
+  // const searchLocation = (event) => {
+  //   if (event.key === 'Enter') {
+  //     axios.get(url).then((response) => {
+  //       setData(response.data)
+  //       console.log(response.data)
+  //     })
+  //     setLocation('')
+  //   }
+  // }
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
-        setData(response.data)
-      })
+      const requestOne = axios.get(url);
+      const requestTwo = axios.get(fiveDay);
+      axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
+        setData(responses[0].data)
+        setFutureData(responses[1].data)
+
+        console.log(responses[0].data, responses[1].data)
+      }))
       setLocation('')
     }
   }
@@ -26,14 +56,26 @@ function WeatherData() {
     })
       setLocation('London')
   }
-  const parisClick = () => {
-    axios.get(parisURL).then((response) => {
+  const tokyoClick = () => {
+    axios.get(tokyoURL).then((response) => {
       setData(response.data)
     })
-      setLocation('Paris')
+      setLocation('Tokyo')
+  }
+  const losAngelesClick = () => {
+    axios.get(losAngelesURL).then((response) => {
+      setData(response.data)
+    })
+      setLocation('Los Angeles')
+  }
+
+  const newYorkClick = () => {
+    axios.get(newYorkURL).then((response) => {
+      setData(response.data)
+    })
+      setLocation('New York')
   }
   
-
     return (
       <React.Fragment>
       <div className="app">
@@ -42,13 +84,13 @@ function WeatherData() {
             onClick={() => londonClick()}
             >London</button>
             <button type="button"
-            onClick={() => parisClick()}
-            >Paris</button>
+            onClick={() => tokyoClick()}
+            >Tokyo</button>
             <button type="button"
-            onClick={() => setLocation('Los Angeles')}
+            onClick={() => losAngelesClick()}
             >Los Angeles</button>
             <button type="button"
-            onClick={() => setLocation('New York')}
+            onClick={() => newYorkClick()}
             >New York</button>
         </div>
         <div className="search">
@@ -72,17 +114,22 @@ function WeatherData() {
                 {data.weather ? <p>{data.weather[0].main}</p> : null}
               </div>
             </div>
+            <div className="middle">
+              {futureData.list ? <p>{new Date(futureData.list[0].dt * 1000).toDateString()}</p> : null}
+              {futureData.list ? <img src={`http://openweathermap.org/img/wn/${futureData.list[0].weather[0].icon}.png`}/> : null}
+              {futureData.list ? <p>{futureData.list[0].main.temp.toFixed()}°F</p> : null}
+            </div>
             <div className="bottom">
-              <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <div className="humidity" >
+              {data.main ? <p className='bold'><UilTear size="20"/>{data.main.humidity}%</p> : null}
                 <p>Humidity</p>
               </div>
               <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
-                <p>Feels Like</p>
+              {data.main ?  <p className='bold'><UilTemperatureThreeQuarter size="20"/> {data.main.feels_like.toFixed()}°F</p> : null}
+                <p> Feels Like</p>
               </div>
               <div className="speed">
-                {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+                {data.wind ? <p className='bold'><UilWind size="20"/>{data.wind.speed.toFixed()} MPH</p> : null}
                 <p>Wind Speed</p>
               </div>
             </div>
